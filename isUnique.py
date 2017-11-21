9f58edf023dc7910c77e2f99a30c7b67c8bb48f0
Ryan Padilla <rjpadil1@uci.edu>
2017-11-21 14:12:32 -0800

    Starting python file

---
+++ isUnique.py
@@ -0,0 +1,12 @@
+def theHashSlingingSlasher(char):
+	return ord(char) % 256
+
+class Node(hashVal):
+	def __init__(self, hashVal):
+		self.val = hashVal
+		self.next = None
+
+class UniqueTester(stringVal):
+	def __init__(self, stringVal):
+		arr = []*256
+		
